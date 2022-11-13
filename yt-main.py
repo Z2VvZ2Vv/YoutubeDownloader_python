140f606212e3c6a3a3d9e77a4f72b1e01bd6a86d
Z2VvZ2Vv <62845771+Z2VvZ2Vv@users.noreply.github.com>
2022-11-13 11:22:51 +0100

    The script himself !

---
+++ yt-main.py
@@ -0,0 +1,27 @@
+def generate_video_filename():
+    return f"{uuid.uuid4()}.mp4"
+
+
+def download_file(url):
+    local_filename = generate_video_filename()
+    with requests.get(url, stream=True) as r:
+        with open(generate_video_filename(), 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+
+    return local_filename
+    
+import requests
+import shutil
+import uuid
+
+#############link de la video ou ID#################
+link = "https://www.youtube.com/watch?v=ID_VIDEO"
+####################################################
+idyt = link.replace("https://www.youtube.com/watch?v=","")
+
+result = requests.get("https://downloader.freemake.com/api/videoinfo/" + idyt)
+resultJson = result.json()
+getQualities = resultJson.get("qualities")[0]
+getUrl = getQualities.get("url")
+download_file(getUrl)
+
